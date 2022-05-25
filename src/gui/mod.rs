54ad283e19bc817a8ee269b581c989e87d1cbc8b
Asami Doi <doiasami1219@gmail.com>
2022-05-26 00:04:21 +0900

    show parsed url

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -2,82 +2,67 @@ mod browser_window;
 
 use browser_window::BrowserWindow;
 use glib::clone;
-use glib::closure_local;
-use gtk4::glib::value::ToValue;
+use gtk4::glib;
 use gtk4::prelude::*;
-use gtk4::{gio, glib};
 use gtk4::{
     Align, Application, Box, HeaderBar, Label, Orientation, SearchBar, SearchEntry, ToggleButton,
 };
 
-pub fn start_browser_window(handle_url: fn(String)) {
+pub fn start_browser_window(handle_url: fn(String) -> String) {
     let application = Application::builder().application_id("vulbr").build();
 
     application.connect_activate(
         clone!(@strong application, @strong handle_url => move |app| {
-            let window = build_ui(app);
-            window.connect_closure(
-                "signal-test",
-                false,
-                closure_local!(move |_w: BrowserWindow, url: String| {
-                    println!("input url: {}", url);
-                    handle_url(url);
-                }),
-            );
+            let window = BrowserWindow::new(app);
+            window.set_default_size(1280, 800);
+            window.set_title(Some("vulbr"));
+
+            let header_bar = HeaderBar::new();
+            window.set_titlebar(Some(&header_bar));
+
+            let search_button = ToggleButton::new();
+            search_button.set_icon_name("system-search-symbolic");
+            search_button.set_active(true);
+            header_bar.pack_end(&search_button);
+
+            let container = Box::new(Orientation::Vertical, 6);
+            window.set_child(Some(&container));
+
+            let search_bar = SearchBar::builder()
+                .valign(Align::Start)
+                .key_capture_widget(&window)
+                .build();
+
+            container.append(&search_bar);
+
+            search_button
+                .bind_property("active", &search_bar, "search-mode-enabled")
+                .flags(glib::BindingFlags::SYNC_CREATE | glib::BindingFlags::BIDIRECTIONAL)
+                .build();
+
+            let entry = SearchEntry::new();
+            entry.set_hexpand(true);
+            search_bar.set_child(Some(&entry));
+
+            let label = Label::builder()
+                .label("Welcome to VulBr!")
+                .vexpand(true)
+                .halign(Align::Center)
+                .valign(Align::Center)
+                .css_classes(vec!["large-title".to_string()])
+                .build();
+
+            container.append(&label);
+
+            entry.connect_activate(clone!(@weak label, @weak window => move |entry| {
+                println!("connect_activate");
+                let parsed_url = handle_url(entry.text().to_string());
+                label.set_label(&parsed_url);
+            }));
+
+            window.show();
         }),
     );
+
     application.run();
-}
-
-fn build_ui(app: &Application) -> BrowserWindow {
-    let window = BrowserWindow::new(app);
-    window.set_default_size(1280, 800);
-    window.set_title(Some("vulbr"));
-
-    let header_bar = HeaderBar::new();
-    window.set_titlebar(Some(&header_bar));
-
-    let search_button = ToggleButton::new();
-    search_button.set_icon_name("system-search-symbolic");
-    search_button.set_active(true);
-    header_bar.pack_end(&search_button);
-
-    let container = Box::new(Orientation::Vertical, 6);
-    window.set_child(Some(&container));
-
-    let search_bar = SearchBar::builder()
-        .valign(Align::Start)
-        .key_capture_widget(&window)
-        .build();
-
-    container.append(&search_bar);
-
-    search_button
-        .bind_property("active", &search_bar, "search-mode-enabled")
-        .flags(glib::BindingFlags::SYNC_CREATE | glib::BindingFlags::BIDIRECTIONAL)
-        .build();
-
-    let entry = SearchEntry::new();
-    entry.set_hexpand(true);
-    search_bar.set_child(Some(&entry));
-
-    let label = Label::builder()
-        .label("Type to start search")
-        .vexpand(true)
-        .halign(Align::Center)
-        .valign(Align::Center)
-        .css_classes(vec!["large-title".to_string()])
-        .build();
-
-    container.append(&label);
-
-    entry.connect_activate(clone!(@weak label, @weak window => move |entry| {
-        println!("connect_activate");
-        window.set_property("url", entry.text());
-        window.emit_by_name::<()>("signal-test", &[&entry.text().to_value()]);
-    }));
-
-    window.show();
-
-    window
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-mod default_page;
 mod gui;
 mod http;
 mod net;
 mod renderer;
 mod url;
 
-use crate::renderer::render;
 use crate::url::ParsedUrl;
 
-fn handle_url(url: String) {
+fn handle_url(url: String) -> String {
     println!("handle_url : {}", url);
+
+    let parsed_url = ParsedUrl::new(url.to_string());
+    println!("parsed_url : {:?}", parsed_url);
+
+    format!("{:?}", parsed_url)
 }
 
 fn main() {
     gui::start_browser_window(handle_url);
+
     /*
-    let parsed_url = ParsedUrl::new(url.to_string());
     let response = udp_response(&parsed_url);
     render(response, &app);
     */
